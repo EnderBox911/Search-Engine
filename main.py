a0d76b2714653bb0470886bbed2174451956e074
EnderBox911 <126318660+EnderBox911@users.noreply.github.com>
2023-02-25 19:54:21 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Webserver
+import flask
+# Data analysis library
+import pandas
+# Connects to webservers and downloads html
+import requests
+# Allows to parse html and extract data
+from bs4 import BeautifulSoup
+
+from app import run_page
+
+# Make sure to install packages at first
+
+run_page()
