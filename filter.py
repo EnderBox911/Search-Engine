52d5be537ff285e7e326376737b7f4cb03217364
EnderBox911 <126318660+EnderBox911@users.noreply.github.com>
2023-03-02 15:31:30 +0700

    Added time tracker to filter to see how long it takes

--- filter.py
+++ filter.py
@@ -1,10 +1,18 @@
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from settings import *
-import urllib
+import urllib, time
 
 with open("blacklist.txt") as f:
+  # Start time
+  start_time_list = time.time()
+  
   bad_domain_list = set(f.read().split("\n"))
+  
+  # End time to open list
+  end_time_list = time.time()
+  list_elapsed = end_time_list - start_time_list
+  print("OPEN BLACKLIST FILE:", list_elapsed)
 
 
 def get_page_content(row):
@@ -14,20 +22,55 @@ def get_page_content(row):
   return text
 
 def tracker_urls(row):
+  overall_time_start = time.time()
+  
+  #Start time to check for srcs
+  start_time_src = time.time()
+  
   soup = BeautifulSoup(row["html"], features="html5lib")
   # Finds everything that has the script tag
   scripts = soup.find_all("script", {"src": True})
   srcs = [s.get("src") for s in scripts]
-
+  
+  # End time for checking for srcs
+  end_time_src = time.time()
+  src_elapsed = end_time_src - start_time_src
+  print("SRC TIME", src_elapsed)
+  
+  
+  #Start time to check for links
+  start_time_lnk = time.time()
+  
   # Finds everything that has the link tag
   links = soup.find_all("a", {"href": True})
   href = [l.get("href") for l in links]
+  
+  # End time for checking for links
+  end_time_lnk = time.time()
+  lnk_elapsed = end_time_lnk - start_time_lnk
+  print("LNK TIME", lnk_elapsed)
+
+  
 
   # Parses it and leaves just the root domain that the src or link is pointig to
   all_domains = [urlparse(s).hostname for s in srcs + href]
 
+  
+  #Start time to check for bad stuff
+  start_time_bad = time.time()
+  
   bad_domains = [a for a in all_domains if a in bad_domain_list]
   
+  # End time for checking for links
+  end_time_bad = time.time()
+  bad_elapsed = end_time_bad - start_time_bad
+  print("BAD TIME", bad_elapsed)
+  
+  overall_time_end = time.time()
+  overall = overall_time_end - overall_time_start
+  print("OVERALL", overall)
+  
+   
   return len(bad_domains)
   
 
